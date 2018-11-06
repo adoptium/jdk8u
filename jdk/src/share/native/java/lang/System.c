c9b3325be7e1b236b55d888b5eca367c06d87cb4
John Oliver <johno@insightfullogic.com>
2018-11-06 10:47:15 +0000

    Set vendor information

--- jdk/src/share/native/java/lang/System.c
+++ jdk/src/share/native/java/lang/System.c
@@ -110,13 +110,13 @@ Java_java_lang_System_identityHashCode(JNIEnv *env, jobject this, jobject x)
 
 /* Third party may overwrite these values. */
 #ifndef VENDOR
-#define VENDOR "Oracle Corporation"
+#define VENDOR "Eclipse Foundation"
 #endif
 #ifndef VENDOR_URL
-#define VENDOR_URL "http://java.oracle.com/"
+#define VENDOR_URL "https://adoptium.net/"
 #endif
 #ifndef VENDOR_URL_BUG
-#define VENDOR_URL_BUG "http://bugreport.sun.com/bugreport/"
+#define VENDOR_URL_BUG "https://github.com/adoptium/adoptium-support/issues"
 #endif
 
 #define JAVA_MAX_SUPPORTED_VERSION 52
